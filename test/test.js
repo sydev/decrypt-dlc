ab94ce388789d8ecaabfb7986360bc121dd604c0
Dominik Winter <dominik.winter92@gmail.com>
2016-12-01 15:27:08 +0100

    (Add) ava as test engine

--- test/test.js
+++ test/test.js
@@ -1,23 +1,6 @@
-(() => {
+import test from 'ava';
+import decrypt from '../index';
 
-  const chai      = require('chai');
-  const decrypt   = require('../index');
-
-  const expect    = chai.expect;
-  const dlc_file  = './test/test.dlc';
-
-  describe('decrypt test.dlc', () => {
-
-    it('should have success string and links array', (done) => {
-      decrypt(dlc_file, (err, response) => {
-        expect(err).to.be.null;
-        expect(response).to.be.object;
-        expect(response.success).to.be.string;
-        expect(response.links).to.be.array;
-        done();
-      });
-    });
-
-  });
-
-})();
+test.cb('decrypt', t => {
+  decrypt('test/test.dlc', t.end);
+});
