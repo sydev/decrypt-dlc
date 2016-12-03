49a2b55ec8d0aa8adde5ed6ed64215866f1b3029
Dominik Winter <dominik.winter92@gmail.com>
2016-12-03 18:50:46 +0100

    (Add) endpoints for container, cnl and paste

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
 (() => {
   'use strict';
 
-  const fs          = require('fs');
-  const path        = require('path');
-  const request     = require('request');
+  const fs      = require('fs');
+  const path    = require('path');
+  const request = require('request');
+  const url     = require('url');
 
-  const DCRYPT_URL  = 'http://dcrypt.it/decrypt/upload';
+  const DCRYPT_URL    = 'http://dcrypt.it/';
+  const UPLOAD_URL    = url.resolve(DCRYPT_URL, '/decrypt/upload');
+  const CONTAINER_URL = url.resolve(DCRYPT_URL, '/decrypt/container');
+  const CNL_URL       = url.resolve(DCRYPT_URL, '/decrypt/cnl');
+  const PASTE_URL     = url.resolve(DCRYPT_URL, '/decrypt/paste');
 
   /**
    * Decrypt a DLC file with the awesome http://dcrypt.it website
    * @param  {String}   file path to the DLC file
    * @param  {Function} cb   callback function
    */
-  function decrypt(file, cb) {
+  function upload(file, cb) {
     let file_path = path.join(process.cwd(), file),
       file_name   = path.basename(file);
 
     // Upload the DLC file
     request.post({
-      url: DCRYPT_URL,
+      url: UPLOAD_URL,
       formData: {
         dlcfile: {
           value: fs.createReadStream(file_path),
@@ -27,6 +32,66 @@
             contentType: 'application/octet-stream'
           }
         }
+      }
+    }, (err, res, body) => {
+      if (err) return cb(err, null);
+
+      // parse the response body to extract the links
+      parseResponseBody(res.toJSON().body, cb);
+    });
+  }
+
+  /**
+   * [url description]
+   * @param  {[type]}   url [description]
+   * @param  {Function} cb  [description]
+   * @return {[type]}       [description]
+   */
+  function container(url, cb) {
+    request.post({
+      url: CONTAINER_URL,
+      formData: {
+        link: url
+      }
+    }, (err, res, body) => {
+      if (err) return cb(err, null);
+
+      // parse the response body to extract the links
+      parseResponseBody(res.toJSON().body, cb);
+    });
+  }
+
+  /**
+   * [cnl description]
+   * @param  {[type]}   url [description]
+   * @param  {Function} cb  [description]
+   * @return {[type]}       [description]
+   */
+  function cnl(url, cb) {
+    request.post({
+      url: CNL_URL,
+      formData: {
+        link: url
+      }
+    }, (err, res, body) => {
+      if (err) return cb(err, null);
+
+      // parse the response body to extract the links
+      parseResponseBody(res.toJSON().body, cb);
+    });
+  }
+
+  /**
+   * [paste description]
+   * @param  {[type]}   content [description]
+   * @param  {Function} cb      [description]
+   * @return {[type]}           [description]
+   */
+  function paste(content, cb) {
+    request.post({
+      url: PASTE_URL,
+      formData: {
+        content: content
       }
     }, (err, res, body) => {
       if (err) return cb(err, null);
@@ -53,5 +118,10 @@
     cb(error, json);
   }
 
-  module.exports = exports = decrypt;
+  module.exports = exports = {
+    upload: upload,
+    container: container,
+    cnl: cnl,
+    paste: paste
+  };
 })();
--- test/test.js
+++ test/test.js
@@ -1,6 +1,23 @@
+import fs from 'fs';
 import test from 'ava';
-import decrypt from '../index';
+import {upload, container, cnl, paste} from '../index';
 
-test.cb('decrypt', t => {
-  decrypt('test/test.dlc', t.end);
+const TEST_FILE       = 'test/test.dlc';
+const TEST_CONTAINER  = 'https://raw.githubusercontent.com/sydev/decrypt-dlc/master/test/test.dlc';
+const TEST_CNL        = 'http://filecrypt.cc/Container/0B81F6C6DF.html';
+
+test.cb('upload', t => {
+  upload(TEST_FILE, t.end);
+});
+
+test.cb('container', t => {
+  container(TEST_CONTAINER, t.end);
+});
+
+test.cb('cnl', t => {
+  cnl(TEST_CNL, t.end);
+});
+
+test.cb('paste', t => {
+  paste(fs.readFileSync(TEST_FILE, 'utf-8'), t.end);
 });
